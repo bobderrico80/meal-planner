7176be3c58be4f401db18003ab867f894ae04b5e
Bob D'Errico <bobderrico80@gmail.com>
2021-10-17 10:19:28 -0400

    Stub out login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,36 +1,49 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './containers/app/app.component';
+import { FeatureComponent } from './containers/feature/feature.component';
+import { LoginComponent } from './containers/login/login.component';
 
 const routes: Routes = [
   {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
     path: '',
-    redirectTo: '/list',
-    pathMatch: 'full',
-  },
-  {
-    path: 'list',
-    loadChildren: () =>
-      import('./features/list/list.module').then((m) => m.ListModule),
-  },
-  {
-    path: 'meal',
-    loadChildren: () =>
-      import('./features/meal/meal.module').then((m) => m.MealModule),
-  },
-  {
-    path: 'plan',
-    loadChildren: () =>
-      import('./features/plan/plan.module').then((m) => m.PlanModule),
-  },
-  {
-    path: 'recipe',
-    loadChildren: () =>
-      import('./features/recipe/recipe.module').then((m) => m.RecipeModule),
-  },
-  {
-    path: 'store',
-    loadChildren: () =>
-      import('./features/store/store.module').then((m) => m.StoreModule),
+    component: FeatureComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/list',
+        pathMatch: 'full',
+      },
+      {
+        path: 'list',
+        loadChildren: () =>
+          import('./features/list/list.module').then((m) => m.ListModule),
+      },
+      {
+        path: 'meal',
+        loadChildren: () =>
+          import('./features/meal/meal.module').then((m) => m.MealModule),
+      },
+      {
+        path: 'plan',
+        loadChildren: () =>
+          import('./features/plan/plan.module').then((m) => m.PlanModule),
+      },
+      {
+        path: 'recipe',
+        loadChildren: () =>
+          import('./features/recipe/recipe.module').then((m) => m.RecipeModule),
+      },
+      {
+        path: 'store',
+        loadChildren: () =>
+          import('./features/store/store.module').then((m) => m.StoreModule),
+      },
+    ],
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MatListModule } from '@angular/material/list';
+import { AppComponent } from './containers/app/app.component';
+import { FeatureComponent } from './containers/feature/feature.component';
+import { LoginComponent } from './containers/login/login.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LoginComponent, FeatureComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -20,6 +25,9 @@ import { MatListModule } from '@angular/material/list';
     MatButtonModule,
     MatSidenavModule,
     MatListModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
