3daecb0eb7df754fbb64e92b3dd0aa0295a0e5ed
Bob D'Errico <bobderrico80@gmail.com>
2021-10-17 11:50:19 -0400

    Stub out list component

--- src/app/features/list/list.module.ts
+++ src/app/features/list/list.module.ts
@@ -1,17 +1,21 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { ListRoutingModule } from './list-routing.module';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { ListComponent } from './containers/list/list.component';
-
+import { ListRoutingModule } from './list-routing.module';
 
 @NgModule({
-  declarations: [
-    ListComponent
-  ],
+  declarations: [ListComponent],
   imports: [
     CommonModule,
-    ListRoutingModule
-  ]
+    ListRoutingModule,
+    MatListModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
-export class ListModule { }
+export class ListModule {}
